e9812b77127e0606e9f09c149f03874b4568a723
Bryan Gwendo <bryangwendo314@gmail.com>
2026-02-09 18:36:39 +0300

    Added function to go click signup for new users

---
+++ entry.js
@@ -0,0 +1,9 @@
+function showSignup() {
+    document.querySelector(".Login").style.display = "none";
+    document.querySelector(".SignUp").style.display = "block";
+}
+
+function showLogin() {
+    document.querySelector(".SignUp").style.display = "none";
+    document.querySelector(".Login").style.display = "block";
+}
